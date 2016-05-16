faa342eea158c20f46a592d65f038873d1691bf8
Vasiliy Kharitonov <vslow@me.com>
2016-05-16 15:10:28 +0300

    gitignore emacs temp files

---
+++ Sources/SWAKit-interface-UIKit/makeInterface.swift
@@ -0,0 +1,13 @@
+/**
+ ** SWAKit-interface-UIKit
+ ** UIKit Interface maker for SWAKit library
+ **
+ ** © Vasiliy Kharitonov, MIT license
+ ** 2016-05-16
+ **/
+
+import SWAKit
+
+public func makeInterface(WebObject: WebObject) -> String {
+    
+}
